75d3db3131880bffd4d0dd6376b4dd17529316e7
nathantbissell <nathantbissell@gmail.com>
2019-06-14 13:08:58 -0400

    landing page gradient added, grid complete, photo showing

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 import './App.css'
 import Main from './components/main.js'
 
-function App() {
+const App = () => {
   return (
     <div className='demo-big-content'>
       <Layout>
-        <Header title='Title' scroll>
+        <Header className='header-color' title='Title' scroll>
           <Navigation>
             <Link to='/resume'>Resume</Link>
             <Link to='/aboutMe'>About Me</Link>
--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import { Grid, Cell } from 'react-mdl'
 
-export default class landingPage extends Component {
+class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Grid className='landing-grid'>
+          <Cell col={12}>
+            <img
+              src='http://pgbxproducts.pregel2.com/media/catalog/product/cache/10/image/1500x/9df78eab33525d08d6e5fb8d27136e95/p/l/plasmon.png'
+              alt='avatar'
+              className='avatar-img'
+            />
+            <div className='banner-text'>
+              <h1>Full Stack Web Developer</h1>
+
+              <hr />
+              <p>HTML | CSS | React | Express | MongoDB | Express</p>
+              <div className='social-links'>
+                <a href='http://google.com' target='_blank'>
+                  <i className='fab fa-instagram' />
+                </a>
+                <a href='http://google.com' target='_blank'>
+                  <i className='fab fa-linkedin' />
+                </a>
+                <a href='http://google.com' target='_blank'>
+                  <i className='fab fa-github' />
+                </a>
+                <a href='http://google.com' target='_blank'>
+                  <i className='fas fa-inbox' />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
 }
+export default Landing
